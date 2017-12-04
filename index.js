21cb90b1e309612b6e4450b5e55af886042e0fde
orthagonal <orthagonal1@gmail.com>
2017-12-04 14:58:28 -0600

    updating for plugin

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const SlackCommand = require('slack-command').SlackCommand;
+const SlackCommand = require('./slackCommand');
 
 const register = async function(server, options) {
   const slackCommand = new SlackCommand(options.token, options, server);
   server.decorate('server', 'registerSlackCommand', slackCommand.register.bind(slackCommand));
-  await slackCommand.listen();
+  server.route({
+    method: 'POST',
+    path: this.options.routeToListen,
+    handler: slackCommand.handler
+  });
 };
 
 exports.plugin = {
